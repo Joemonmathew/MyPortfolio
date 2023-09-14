2daf51b48681d2fae11b3e6b0004d636fa59eef2
Joemon Mathew <joemonmathew21@gmail.com>
2023-09-14 18:00:28 +0530

    added gap between list items

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -4,13 +4,13 @@ const Sidebar = () => {
   return (
     <div className=' top-0 left-0 h-screen w-1/5 bg-gray-800 text-green-500 p-4 box-border '>
       <h3>Joemon Mathew</h3>
-      <ul className='flex flex-col items-center justify-center mt-4'>
-        <li className='mt-4'><a>About</a></li>
-        <li className='mt-4'><a>Work</a> </li>
-        <li className='mt-4'><a>Skills</a></li>
-        <li className='mt-4'><a>Resume</a></li>
-        <li className='mt-4'><a>Blogs</a></li>
-        <li className='mt-4'><a>Contact</a></li>
+      <ul className='flex flex-col items-center justify-center mt-4 space-y-5'>
+        <li><a>About</a></li>
+        <li><a>Work</a> </li>
+        <li><a>Skills</a></li>
+        <li><a>Resume</a></li>
+        <li><a>Blogs</a></li>
+        <li><a>Contact</a></li>
       </ul>
     </div>
   );
